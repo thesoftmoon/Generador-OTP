954e220db2fa59bc4d9f1d72e0225a322c94d9c3
Tomás <tomas.pacheco.c@gmail.com>
2023-02-03 17:33:18 -0300

    solved the copy to clipboard script

--- app.js
+++ app.js
@@ -5,16 +5,38 @@ const copyBtn = document.querySelector('.clipboard-btn');
 function generadorOtp (){
     const numeros = '0123456789'; 
     let otp = '';
-
+    
     for(let i = 0; i < 6; i++){
         otp += numeros[Math.floor(Math.random() * 10)];
     }
     return numbs.innerHTML = `<h3>${otp}</h3>`;
 }
 
+const copyCode = async ()=>{
+    const otpCode = numbs.textContent;
+    try{
+        navigator.clipboard.writeText(otpCode);
+        console.log('Se copio el codigo: ' + otpCode)
+    }
+    catch(err){
+        console.log('No se copio el texto')
+    }
+}    
+    
+
 copyBtn.addEventListener('click', function (){
-    console.log('presionaste el boton de copiar');
+    //copyClipboard()
+    copyCode()
 })
+
+navigator.clipboard.writeText("This is the text to be copied").then(() => {
+    console.log('Content copied to clipboard');
+    /* Resolved - text copied to clipboard successfully */
+  },() => {
+    console.error('Failed to copy');
+    /* Rejected - text failed to copy to the clipboard */
+  });
+
 
 generadorOtp();
 setInterval(generadorOtp, 6000);
